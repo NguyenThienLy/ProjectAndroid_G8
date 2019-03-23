d9e1480a4a2db1bf69933e09b9ca4c063061e3e9
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-03-23 21:32:08 +0700

    update name component control

--- DesignAppRoot/app/src/main/java/com/example/designapptest/MainActivity.java
+++ DesignAppRoot/app/src/main/java/com/example/designapptest/MainActivity.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
-    GridView gvRoom;
-    ListView lstRoom;
+    GridView grVRoom;
+    ListView lstVRoom;
     ArrayList<roomModel> mydata;
     com.example.designapptest.roomAdapter roomAdapterGid;
     com.example.designapptest.roomAdapter roomAdapterList;
@@ -19,19 +19,21 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AnhXa();
+        initData();
         roomAdapterGid=new roomAdapter(this,R.layout.rom_element_grid_view,mydata);
         roomAdapterList=new roomAdapter(this,R.layout.rom_element_list_view,mydata);
-        gvRoom.setAdapter(roomAdapterGid);
-        lstRoom.setAdapter(roomAdapterList);
-        gvRoom.setClickable(false);
-        lstRoom.setClickable(false);
+        grVRoom.setAdapter(roomAdapterGid);
+        lstVRoom.setAdapter(roomAdapterList);
+        grVRoom.setClickable(false);
+        lstVRoom.setClickable(false);
     }
-    private void AnhXa()
+    private void initData()
     {
-        gvRoom=(GridView) findViewById(R.id.gv_room);
-        lstRoom=(ListView) findViewById(R.id.lst_room);
+        grVRoom=(GridView) findViewById(R.id.grV_room);
+        lstVRoom=(ListView) findViewById(R.id.lstV_room);
+
         mydata=new ArrayList<>();
+
         mydata.add(new roomModel(R.drawable.room_1,"Cho thuê phòng trọ giá rẻ","2.5 triệu/phòng","54 Âu Cơ, Bình Thạnh, TP Hồ Chí Minh", 8, 256, "PHÒNG TRỌ"));
         mydata.add(new roomModel(R.drawable.room_1,"Cho thuê phòng trọ giá rẻ","3.5 triệu/phòng","54 Âu Cơ, Quận 11, TP Hồ Chí Minh", 6, 18, "PHÒNG TRỌ"));
         mydata.add(new roomModel(R.drawable.room_1,"Cho thuê phòng trọ giá rẻ","2.5 triệu/phòng","54 Âu Cơ, Bình Thạnh, TP Hồ Chí Minh", 5, 365, "CHUNG CƯ"));
