b5a166a8ea9346a3ef6071085b5a26ee1fb6b295
Sinh <trinhnhatsinh123@gmail.com>
2019-04-03 16:23:44 +0700

    And file suggestAdapter

---
+++ DesignAppRoot/app/src/main/java/com/example/designapptest/suggestAdapter.java
@@ -0,0 +1,53 @@
+package com.example.designapptest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class suggestAdapter extends BaseAdapter {
+    private Context context;
+    private int layout;
+    private List<roomModel> room;
+
+    public suggestAdapter(Context context, int layout, List<roomModel> room) {
+        this.context = context;
+        this.layout = layout;
+        this.room = room;
+    }
+    @Override
+    public int getCount() {
+        return room.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater= (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView= inflater.inflate(layout,null);
+        //anh xa
+        ImageView image=(ImageView) convertView.findViewById(R.id.img_room);
+        TextView name=(TextView) convertView.findViewById(R.id.txtV_name);
+        TextView address=(TextView) convertView.findViewById(R.id.txtV_address);
+        //gan du lieu
+        roomModel itemroom=room.get(position);
+        image.setImageResource(itemroom.getImage());
+        name.setText(itemroom.getName());
+        address.setText(itemroom.getAddress());
+        return convertView;
+    }
+}
