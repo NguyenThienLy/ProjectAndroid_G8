2c7203bec5554f31604df91925f5f54cb4edf72f
quang <1612534@student.hcmus.edu.vn>
2019-04-17 01:56:51 +0700

    sua login va post room (post room chua test)

--- DesignAppRoot/app/src/main/java/com/example/designapptest/postRoomStep1.java
+++ DesignAppRoot/app/src/main/java/com/example/designapptest/postRoomStep1.java
@@ -3,33 +3,33 @@ package com.example.designapptest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class postRoomStep1 extends Activity{
-    Button btnStep1PostRoom;
+public class postRoomStep1 extends Fragment{
+
+    public postRoomStep1(){
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.post_room_step_1_view);
-
-        initControl();
-
-        nextStep();
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.post_room_main_view);
+//
+//       // initControl();
+//
+//      //  nextStep();
+//    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.post_room_step_1_view, container, false);
     }
 
-    private void initControl() {
-        btnStep1PostRoom = (Button) findViewById(R.id.btn_nextStep1_post_room);
-    }
 
-    private  void nextStep() {
-        btnStep1PostRoom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), postRoomStep2.class);
-                startActivity(intent);
-            }
-        });
-    }
 }
--- DesignAppRoot/app/src/main/java/com/example/designapptest/postRoomStep3.java
+++ DesignAppRoot/app/src/main/java/com/example/designapptest/postRoomStep3.java
@@ -3,33 +3,26 @@ package com.example.designapptest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class postRoomStep3 extends Activity {
-    Button btnStep3PostRoom;
+public class postRoomStep3 extends Fragment{
+
+    public postRoomStep3(){
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.post_room_step_3_view);
-
-        initControl();
-
-        nextStep();
+//
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.post_room_step_3_view, container, false);
     }
 
-    private void initControl() {
-        btnStep3PostRoom = (Button) findViewById(R.id.btn_nextStep3_post_room);
-    }
 
-    private  void nextStep() {
-        btnStep3PostRoom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), postRoomStep4.class);
-                startActivity(intent);
-            }
-        });
-    }
 }
