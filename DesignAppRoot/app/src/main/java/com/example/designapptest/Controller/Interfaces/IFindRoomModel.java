84d3f554f486064c41ade6d40977234c352e59c3
Nguyen Thien Ly <nguyenmit2012@gmail.com>
2019-05-05 00:48:43 +0700

    add find room and detail find room have bug

---
+++ DesignAppRoot/app/src/main/java/com/example/designapptest/Controller/Interfaces/IFindRoomModel.java
@@ -0,0 +1,7 @@
+package com.example.designapptest.Controller.Interfaces;
+
+import com.example.designapptest.Model.FindRoomModel;
+
+public interface IFindRoomModel {
+    public void getListFindRoom(FindRoomModel valueRoom);
+}
