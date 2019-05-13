2eadac8da55cbb2394c97bee91b07353cc315feb
Sinh <trinhnhatsinh123@gmail.com>
2019-05-13 17:08:39 +0700

    sua giao dien chinh

--- DesignAppRoot/app/src/main/java/com/example/designapptest/Views/roomManagementModel.java
+++ DesignAppRoot/app/src/main/java/com/example/designapptest/Views/roomManagementModel.java
@@ -2,6 +2,7 @@ package com.example.designapptest.Views;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +20,13 @@ import com.example.designapptest.Model.ConvenientModel;
 import com.example.designapptest.Model.RoomModel;
 import com.example.designapptest.Model.UserModel;
 import com.example.designapptest.R;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,6 +44,7 @@ public class roomManagementModel extends AppCompatActivity {
     RecyclerView recyclerMainRoom;
     MainActivityController mainActivityController;
     List<RoomModel> roomModelList = new ArrayList<>();
+    PieChart pieChart;
 
     private Integer[] viewData = {356,75,684,464};
     private String[] nameData = {"Phòng trọ 1","Phòng trọ 2","Phòng trọ 3","Phòng trọ 4"};
@@ -46,12 +55,62 @@ public class roomManagementModel extends AppCompatActivity {
         setContentView(R.layout.room_management_user_view);
 
         recyclerMainRoom = (RecyclerView)findViewById(R.id.recycler_Main_Room);
+        pieChart=(PieChart) findViewById(R.id.chart_view);
+
+        pieChart.setRotationEnabled(true);
+        pieChart.setHoleRadius(25f);
+        pieChart.setTransparentCircleAlpha(0);
+        pieChart.setCenterText("Biểu đồ View");
+        pieChart.setCenterTextSize(10);
+
+        addDataSet(pieChart);
+        pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+            @Override
+            public void onValueSelected(Entry e, Highlight h) {
+                int pos = Math.round(h.getX());
+                Toast.makeText(roomManagementModel.this,nameData[pos],Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected() {
+
+            }
+        });
 
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
         SharedPreferences sharedPreferences2 = this.getSharedPreferences(LoginView.PREFS_DATA_NAME, MODE_PRIVATE);
         UID = sharedPreferences2.getString(LoginView.SHARE_UID,"n1oc76JrhkMB9bxKxwXrxJld3qH2");
 
+    }
+    private void addDataSet(PieChart pieChart) {
+        ArrayList<PieEntry> yEntry=new ArrayList<>();
+        ArrayList<String> xEntry=new ArrayList<>();
+
+        for (int i=0;i<viewData.length;i++)
+        {
+            yEntry.add(new PieEntry(viewData[i],i));
+        }
+        for (int i=0;i<nameData.length;i++)
+        {
+            xEntry.add(nameData[i]);
+        }
+
+        PieDataSet pieDataSet=new PieDataSet(yEntry,"Số người xem");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        ArrayList<Integer> colors=new ArrayList<>();
+        colors.add(Color.rgb(0,255,255));
+        colors.add(Color.rgb(255,0,0));
+        colors.add(Color.rgb(255,255,0));
+        colors.add(Color.rgb(255,0,255));
+
+        pieDataSet.setColors(colors);
+
+        PieData pieData =new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
     }
 
     @Override
